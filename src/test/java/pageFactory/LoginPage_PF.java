924905e66b6cc3d3cac88617d069873c871d6b76
Dinesh Borude <borudedinesh2@gmail.com>
2024-12-10 01:39:48 +0530

    sandy's fix

--- src/test/java/pageFactory/LoginPage_PF.java
+++ src/test/java/pageFactory/LoginPage_PF.java
@@ -8,29 +8,32 @@ import org.openqa.selenium.support.PageFactory;
 public class LoginPage_PF {
     WebDriver driver;
 
+    // Constructor to initialize WebDriver and PageFactory
     public LoginPage_PF(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(driver, LoginPage_PF.class);
+        PageFactory.initElements(driver, this);
     }
 
+    // Locators
     @FindBy(id = "user-name")
-    WebElement text_UserName ;
+    WebElement text_UserName;
 
     @FindBy(id = "password")
     WebElement text_Password;
 
-    @FindBy(id="login-button")
+    @FindBy(id = "login-button")
     WebElement button_Login;
 
-    @FindBy(xpath = "div[class='product_label']")
+    @FindBy(xpath = "//div[@class='product_label']")  // Corrected XPath
     WebElement pageHeader;
 
+    // Actions
     public void enterUserName(String username) {
         text_UserName.sendKeys(username);
     }
 
     public void enterPassword(String password) {
-       text_Password.sendKeys(password);
+        text_Password.sendKeys(password);
     }
 
     public void clickLogin() {
